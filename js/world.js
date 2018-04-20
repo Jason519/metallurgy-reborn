19971ecee3cb467df19cc216364d90f36b0acea9
Jason <jasonhernandez915@gmail.com>
2018-04-20 13:11:11 -0700

    newLevel implementation; loads data and checks for other scripts

--- js/world.js
+++ js/world.js
@@ -1,4 +1,7 @@
 var enable_mod;
+var block_data = [];
+var item_data = [];
+
 var mod_directory = "/games/com.mojang/minecraftpe/mods/metallurgy/";
 
 Level.saveWorldFile = function(filename, content){try {java.io.File(android.os.Environment.getExternalStorageDirectory().getPath() + "/games/com.mojang/minecraftWorlds/" + Level.getWorldDir() + "/").mkdirs();var newFile = new java.io.File(android.os.Environment.getExternalStorageDirectory().getPath() + "/games/com.mojang/minecraftWorlds/" + Level.getWorldDir() + "/", filename);newFile.createNewFile();var outWrite = new java.io.OutputStreamWriter(new java.io.FileOutputStream(newFile));outWrite.append(content);outWrite.close();} catch (err) {print(err);}};
@@ -11,8 +14,12 @@ function newLevel(){
 		for(var i = 0; i < scripts.size(); i++){
 			var script = scripts[i];
 			if(script.name == "blocks" && script.name == "items"){
-      			enable_mod = true;
-    		}
+      				enable_mod = true;
+				let json_block_data = JSON.parse(ModPE.readFile(mod_directory + "blocks.json"));
+				block_data.push(json_block_data);
+				let json_item_data = JSON.parse(ModPE.readFile(mod_directory + "items.json"));
+				item_data.push(json_item_data);
+    			}
   		}
 	}
 }
@@ -21,6 +28,8 @@ function modTick(){
 		OreArray[i].generate();
 	}
 };
+
+
 
 var OreArray = new function(){
 	
