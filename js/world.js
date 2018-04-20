9bdf7956d257f195449508c7539661b845791c9b
Jason <jasonhernandez915@gmail.com>
2018-04-20 08:03:45 -0700

    not finished, commiting so i dont forget

--- js/world.js
+++ js/world.js
@@ -1,1 +1,11 @@
+var enable_mod = false;
 
+function newLevel(){
+  var scripts = com.zhuoweizhang.mcpelauncher.mcpelauncher.ScriptManager.script();
+  for(var i = 0; i < scripts.size(); i++){
+    var script = scripts[i];
+    if(script.name == "blocks" && script.name == "items"){
+      enable_mod = true;
+    }
+  }
+}
