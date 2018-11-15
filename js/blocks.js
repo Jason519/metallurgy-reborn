e9a93d0bd1988297da4d1c95f348ee22d1e625d1
Jason519 <jason.hernandez1895@email.bakersfieldcollege.edu>
2018-11-15 14:14:41 -0800

    Update blocks.js

--- js/blocks.js
+++ js/blocks.js
@@ -1,1 +1,18 @@
+//[0] ore, [1] block, [2] brick, [3] liquid
+var Metal = {}
+Metal.Alloy =  function([blockID], alloy_name){
+	this.BlockIDArray = blockID;
+	this.alloy_name = alloy_name;
+	Block.defineBlock(this.BlockIDArray[0], this.alloy_name + " Ore", [/*texture array*/]);
+    Block.defineBlock(this.BlockIDArray[1], this.alloy_name + " Block", [/*texture array*/]);
+    Block.defineBlock(this.BlockIDArray[2], this.alloy_name + " Brick", [/*texture array*/]);
+    Block.defineLiquidBlock(this.BlockIDArray[3], "Molten " + this.alloy_name, [/*texture array*/]);
+};
 
+var alloy = Metal.Alloy;
+
+alloy.prototype.setDestroyTime = function(time){
+	for(i in this.BlockIDArray){
+		Block.setDestroyTime(this.BlockIDArray[i], time);
+	}
+}
